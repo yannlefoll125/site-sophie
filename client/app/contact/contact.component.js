d686a0847e32e96602fce63f5b8c3c53270e4b72
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2017-03-31 13:46:45 +0200

    contact: form is reset after being submitted

--- client/app/contact/contact.component.js
+++ client/app/contact/contact.component.js
@@ -8,15 +8,38 @@ import routes from './contact.routes';
 export class ContactComponent {
   /*@ngInject*/
   constructor() {
-    this.message = 'Hello';
+    this.submitted = false;
+    this.values = {};
+  }
+
+  onFormSubmit(form) {
+    this.submitted = true;
+
+
+
+    console.log("onFormSubmit");
+    console.log(this);
+
+    if(form.$valid) {
+      console.log("Form is valid");
+
+      //Resetting form
+      this.values = {};
+      form.$setPristine();
+      form.$setUntouched();
+      this.submitted = false;
+      
+
+    } else {
+      console.log("Form is invalid");
+    }
   }
 }
 
 export default angular.module('siteSophieApp.contact', [ngRoute])
-  .config(routes)
-  .component('contact', {
-    template: require('./contact.html'),
-    controller: ContactComponent,
-    controllerAs: 'contactCtrl'
-  })
-  .name;
+.config(routes)
+.component('contact', {
+  template: require('./contact.html'),
+  controller: ContactComponent
+})
+.name;
