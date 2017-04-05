ac9b24821d1cae9215895ba8dd8ef1c4865cafa7
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2017-04-05 15:44:41 +0200

    added info on header

--- client/app/directives/imgHeader/imgHeader.directive.js
+++ client/app/directives/imgHeader/imgHeader.directive.js
@@ -8,31 +8,55 @@ export default angular.module('siteSophieApp.imgHeader', [])
 		restrict: 'E',
 		link: function(scope, element, attrs) {
 
-			var imageSrc = attrs['imageSrc'];
-			var ratio = attrs['ratio'];
+			let imageSrc = attrs['imageSrc'];
+			let ratio = attrs['ratio'];
 
-			var header = element.find('header');
-			var bottomDiv = angular.element(document.getElementById('bottom'));
+			let header = element.find('header');
+			let bottomDiv = angular.element(document.getElementById('bottom'));
+			let rightDiv = angular.element(document.getElementById('right'));
 
-			scope.resizeHeader = function() {
-				var headerWidth = header[0].clientWidth;
-				var headerHeight = (headerWidth * ratio);
+			const bottomDivHeightRatio = 0.2;
+			const bottomDivHeightRatioComp = 1 - bottomDivHeightRatio;
+
+			scope.updateHeader = function() {
+				let headerWidth = header[0].clientWidth;
+				let headerHeight = (headerWidth * ratio);
 
 				header.css('height', headerHeight + 'px');
 
-				bottomDiv.css('font-size', (0.18 * headerHeight) + 'px');
+				let bottomDivHeight = headerHeight * bottomDivHeightRatio;
+				bottomDiv.css('height', bottomDivHeight + 'px');
+				bottomDiv.css('font-size', 0.7 * bottomDivHeight + 'px');
 
+				
+				let rightDivHeight = rightDiv[0].clientHeight;
+
+				/*
+				if(headerHeight < rightDivHeight + bottomDivHeight) {
+					console.log(false);
+					scope.$apply(function() {
+						scope.showBottomDiv = false;
+					});
+					
+				} else {
+					console.log(true);
+					scope.$apply(function() {
+						scope.showBottomDiv = true;
+					});
+				}
+				*/
 			}
 
 			angular.element($window).on('resize', function() {
-				scope.resizeHeader();
+				scope.updateHeader();
+
 			});
 
 			/*header.css('width', '100%');
 			header.css('background-repeat', 'no-repeat');
 			header.css('background-size', '100%');*/
 			header.css('background-image', 'url("' + imageSrc + '")');
-			scope.resizeHeader();
+			scope.updateHeader();
 
 		}
 	};
