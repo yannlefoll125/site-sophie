cd98efb8a87a02859c13f8b6582925d918bf120e
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2017-04-06 22:15:28 +0200

    App: the title changes according to the page

--- client/app/pageController/pageController.controller.js
+++ client/app/pageController/pageController.controller.js
@@ -5,26 +5,42 @@ const angular = require('angular');
 export function pageController($scope, $location) {
 	var self = this;
 
+	this.pageParams = {
+		'/accueil': {
+			image: '../assets/images/header.jpg',
+			title: 'Accueil'
+		},
+		'/gestalt': {
+			image: '../assets/images/mouette_header.jpg',
+			title: 'Gestalt Thérapie'
+		},
+		'/autres': {
+			image: '../assets/images/marche_header.jpg',
+			title: 'Autres thérapies'
+		},
+		'/proposition': {
+			image: '../assets/images/header.jpg',
+			title: 'Ma proposition'
+		},
+		'/seance': {
+			image: '../assets/images/phare_header.jpg',
+			title: 'Séances et tarifs'
+		},
+		'/contact': {
+			image: '../assets/images/header.jpg',
+			title: 'Contact'
+		}
+	};
+
 	this.route = $location.path();
-	this.test = 'test string';
-
-	this.headerImage = {
-		'/accueil': '../assets/images/header.jpg',
-		'/gestalt': '../assets/images/mouette_header.jpg',
-		'/autres': '../assets/images/marche_header.jpg',
-		'/proposition': '../assets/images/header.jpg',
-		'/seance': '../assets/images/phare_header.jpg',
-		'/contact': '../assets/images/header.jpg'
-
-
-	}
-
-	this.headerImageUrl = this.headerImage[this.route];
+	this.headerImageUrl = this.pageParams[this.route];
+	this.pageTitle = this.pageParams[this.route];
 
 	$scope.$on('$routeChangeSuccess', function() {
 		console.log($location.path());
 		self.route = $location.path();
-		self.headerImageUrl = self.headerImage[self.route];
+		self.headerImageUrl = self.pageParams[self.route].image;
+		self.pageTitle = self.pageParams[self.route].title;
 	});
 }
 
